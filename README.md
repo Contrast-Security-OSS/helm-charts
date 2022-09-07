08fcbd228133613267122787d0d54128396cfeae
Mark Lopez <m@silvenga.com>
2022-09-07 15:53:54 -0500

    Stripped charts path.

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # helm-charts
 
 This repository contains Helm charts of various Contrast Security products. New chart versions are publish to [contrastsecurity.dev/helm-charts](https://contrastsecurity.dev/helm-charts/).
+
+Using this repository:
+
+```
+# Add this Helm repository:
+helm repo add contrast-security https://contrastsecurity.dev/helm-charts
+helm repo update contrast-security
+
+# Locate available charts.
+helm search repo contrast-security
+
+# Show the supported values for a chart.
+helm show values contrast-security/contrast-agent-operator
+```
--- build.ps1
+++ build.ps1
@@ -7,23 +7,21 @@ param(
 $root = $PSScriptRoot
 $srcPath = [System.IO.Path]::GetFullPath("$root/src")
 $distPath = [System.IO.Path]::GetFullPath($Output)
-$chartsPath = [System.IO.Path]::GetFullPath("$distPath/charts")
 
 # Cleanup
 Write-Host "Cleaning $distPath..."
 New-Item -Path $distPath -ItemType Directory -ErrorAction Ignore | Out-Null
 Remove-Item -Path $distPath/* -Recurse
-New-Item -Path $chartsPath -ItemType Directory | Out-Null
 
 # Stage
 Get-ChildItem -Path $srcPath -Recurse -Filter *.tgz -File | ForEach-Object {
-    Write-Host "Staging $($_.FullName) -> $chartsPath"
-    $_ | Copy-Item -Destination $chartsPath
+    Write-Host "Staging $($_.FullName) -> $distPath"
+    $_ | Copy-Item -Destination $distPath
 }
 
 # Build index.
-Write-Host "Generating charts index for $chartsPath..."
-helm repo index $chartsPath --url $ChartRepositoryUrl
+Write-Host "Generating charts index for $distPath..."
+helm repo index $distPath --url $ChartRepositoryUrl
 
 Write-Host "Generating html index for $distPath..."
 tree -H $ChartRepositoryUrl -L 3 --noreport --charset utf-8 -o $distPath/index.html --du -h -T "Contrast Security, Inc Helm charts" -- $distPath
